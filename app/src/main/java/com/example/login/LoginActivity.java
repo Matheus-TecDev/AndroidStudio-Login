30fe66bafe3c8bf581e02bae68922af75d1fbbd9
Aecia-Moveis <suporte@aeciamoveis.com.br>
2025-11-06 17:18:43 -0300

    refatoração da tela de login e seu estilo

--- app/src/main/java/com/example/login/LoginActivity.java
+++ app/src/main/java/com/example/login/LoginActivity.java
@@ -6,6 +6,7 @@ import android.widget.ImageView;
 import com.bumptech.glide.Glide;
 
 public class LoginActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,7 +16,7 @@ public class LoginActivity extends AppCompatActivity {
 
         Glide.with(this)
                 .asGif()
-                .load(R.raw.avatar_animado) // nome do seu arquivo .gif
+                .load(R.drawable.avatar_circle_bg) // nome exato do teu .gif
                 .into(imgProfile);
     }
 }
