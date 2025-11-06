1a94c15a77bb29063834d24565b11bd4bfbb826b
Aecia-Moveis <suporte@aeciamoveis.com.br>
2025-11-06 15:40:47 -0300

    feat: tela de login base criada e estrutura XML ajustada

--- app/src/main/java/com/example/login/LoginActivity.java
+++ app/src/main/java/com/example/login/LoginActivity.java
@@ -1,4 +1,21 @@
 package com.example.login;
 
-public class LoginActivity {
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import com.bumptech.glide.Glide;
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        ImageView imgProfile = findViewById(R.id.imgProfile);
+
+        Glide.with(this)
+                .asGif()
+                .load(R.raw.avatar_animado) // nome do seu arquivo .gif
+                .into(imgProfile);
+    }
 }
